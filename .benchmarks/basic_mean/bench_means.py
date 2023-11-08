c0f05ba765f08a2f5753c6884185aab437daf308
Robert Scheidegger <robert_scheidegger@brown.edu>
2023-11-07 20:31:10 -0500

    feat: additional benchmarks, almost beating numpy with unrolling

--- .benchmarks/basic_mean/bench_means.py
+++ .benchmarks/basic_mean/bench_means.py
@@ -1,24 +1,67 @@
+import os
+
+N = 10**4
+D = 10**4
+
+# Use these to ensure fairness, since this is all single-threaded
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+
 import numpy as np
 import basic_mean_benchmark
 
-N = 10**3
-D = 10**5
-
-ITERATIONS = 10**0
-
 INPUT_DATA = np.random.rand(N, D).astype(np.float32)
+INPUT_DATA_TRANSPOSED = INPUT_DATA.T.copy()
 
 
 def mean_numpy() -> np.ndarray:
-    return np.mean(INPUT_DATA, axis=0)
+    return np.mean(INPUT_DATA.T, axis=1)
 
 
 def mean_rust_numpy() -> np.ndarray:
     return basic_mean_benchmark.mean_numpy(INPUT_DATA)
 
 
+def mean_rust_native() -> np.ndarray:
+    return basic_mean_benchmark.mean_native(INPUT_DATA)
+
+
+def mean_rust_native_transpose() -> np.ndarray:
+    return basic_mean_benchmark.mean_native_transpose(INPUT_DATA_TRANSPOSED)
+
+
+def mean_rust_native_transpose_unroll() -> np.ndarray:
+    return basic_mean_benchmark.mean_native_transpose_unroll(INPUT_DATA_TRANSPOSED)
+
+
+def mean_rust_native_transpose_blas() -> np.ndarray:
+    return basic_mean_benchmark.mean_native_transpose_blas(INPUT_DATA_TRANSPOSED)
+
+
+def mean_rust_native_blas() -> np.ndarray:
+    return basic_mean_benchmark.mean_native_blas(INPUT_DATA)
+
+
 __benchmarks__ = [
     # (e, mean_naive, "Naive (Python) - Baseline"),
     (mean_numpy, mean_numpy, "Numpy"),
     (mean_numpy, mean_rust_numpy, "Numpy (Rust)"),
+    (mean_numpy, mean_rust_native, "Native (Rust)"),
+    (mean_numpy, mean_rust_native_transpose, "Native (Rust) - Transposed"),
+    (
+        mean_numpy,
+        mean_rust_native_transpose_unroll,
+        "Native (Rust) - Transposed (Unrolled)",
+    ),
+    (mean_numpy, mean_rust_native_transpose_blas, "Native (Rust) - Transposed (BLAS)"),
+    (mean_numpy, mean_rust_native_blas, "Native (Rust) (BLAS)"),
 ]
+
+if __name__ == "__main__":
+    print("Testing correctness of implementations...")
+
+    naive = mean_numpy()
+    native_transposed = np.mean(INPUT_DATA_TRANSPOSED, axis=1)
+
+    for _, f, _ in __benchmarks__:
+        print("Testing", f.__name__, "...", "OK" if np.allclose(naive, f()) else "FAIL")
--- .benchmarks/basic_mean/src/lib.rs
+++ .benchmarks/basic_mean/src/lib.rs
@@ -1,25 +1,203 @@
-use numpy::ndarray::{ArrayD, ArrayViewD, ArrayViewMutD};
+extern crate rblas as blas;
+
+use cblas::sasum;
+use numpy::ndarray::ArrayD;
 use numpy::{IntoPyArray, PyArrayDyn, PyReadonlyArrayDyn};
-use pyo3::IntoPy;
 use pyo3::{pymodule, types::PyModule, PyResult, Python};
 
 #[pymodule]
 fn basic_mean_benchmark<'py>(_py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
     #[pyfn(m)]
     #[pyo3(name = "mean_numpy")]
-    fn mean_numpy<'py>(py: Python<'py>, x: PyReadonlyArrayDyn<'py, f32>) -> Option<f32> {
+    fn mean_numpy<'py>(py: Python<'py>, x: PyReadonlyArrayDyn<'py, f32>) -> &'py PyArrayDyn<f32> {
         let dims = x.shape();
         if dims.len() != 2 {
             panic!("Expected a 2D array!");
         }
 
-        let n = dims[0] as f32;
+        return x
+            .as_array()
+            .mean_axis(numpy::ndarray::Axis(0))
+            .expect("Mean should not fail.")
+            .into_pyarray(py);
+    }
 
-        let a = x.as_array();
-        //.mean_axis(numpy::ndarray::Axis(0));
-        //.expect("Mean should not fail.");
-        //.into_pyarray(py);
-        return Some(0.0);
+    #[pyfn(m)]
+    #[pyo3(name = "mean_native")]
+    fn mean_native<'py>(py: Python<'py>, x: PyReadonlyArrayDyn<'py, f32>) -> &'py PyArrayDyn<f32> {
+        let dims = x.shape();
+        if dims.len() != 2 {
+            panic!("Expected a 2D array!");
+        }
+
+        let x = x.as_array();
+        let n = dims[0];
+        let d = dims[1];
+
+        // Allocate a new array to store the result
+        let mut result = ArrayD::<f32>::zeros(vec![dims[1]]);
+        let input_ptr = x.as_ptr();
+        let input_stride = x.strides()[0] as usize;
+        let result_view = result.as_mut_ptr();
+        for j in 0..d {
+            let mut sum: f32 = 0.0;
+            let output_location = unsafe { result_view.offset((j) as isize) };
+            for i in 0..n {
+                sum += unsafe { *input_ptr.offset((i * input_stride + j) as isize) };
+            }
+            unsafe {
+                *output_location = sum / (n as f32);
+            }
+        }
+
+        return result.into_pyarray(py);
+    }
+
+    #[pyfn(m)]
+    #[pyo3(name = "mean_native_transpose")]
+    fn mean_native_transpose<'py>(
+        py: Python<'py>,
+        x: PyReadonlyArrayDyn<'py, f32>,
+    ) -> &'py PyArrayDyn<f32> {
+        // Assume that the input is transposed, so it is a 2D array with shape (d, n)
+
+        let dims = x.shape();
+        if dims.len() != 2 {
+            panic!("Expected a 2D array!");
+        }
+
+        let x = x.as_array();
+        let d = dims[0];
+        let n = dims[1];
+
+        // Allocate a new array to store the result
+        let mut result = ArrayD::<f32>::zeros(vec![dims[1]]);
+        let input_ptr = x.as_ptr();
+
+        let input_stride = x.strides()[0] as usize;
+        println!("input_strides: {:?}, n = {}, d = {}", x.strides(), n, d);
+        let result_view = result.as_mut_ptr();
+        for j in 0..d {
+            let mut sum: f32 = 0.0;
+            let output_location = unsafe { result_view.offset((j) as isize) };
+            let input_pointer = unsafe { input_ptr.offset((j * input_stride) as isize) };
+            for i in 0..n {
+                sum += unsafe { *input_pointer.offset((i) as isize) };
+            }
+            unsafe {
+                *output_location = sum / (n as f32);
+            }
+        }
+
+        return result.into_pyarray(py);
+    }
+
+    #[pyfn(m)]
+    #[pyo3(name = "mean_native_transpose_unroll")]
+    fn mean_native_transpose_unroll<'py>(
+        py: Python<'py>,
+        x: PyReadonlyArrayDyn<'py, f32>,
+    ) -> &'py PyArrayDyn<f32> {
+        // Assume that the input is transposed, so it is a 2D array with shape (d, n)
+
+        let dims = x.shape();
+        if dims.len() != 2 {
+            panic!("Expected a 2D array!");
+        }
+
+        let x = x.as_array();
+        let d = dims[0];
+        let n = dims[1];
+
+        // Allocate a new array to store the result
+        let mut result = ArrayD::<f32>::zeros(vec![dims[1]]);
+        let input_ptr = x.as_ptr();
+        let input_stride = x.strides()[0] as usize;
+        let result_view = result.as_mut_ptr();
+        for j in 0..d {
+            let mut sum: f32 = 0.0;
+            let output_location = unsafe { result_view.offset((j) as isize) };
+            let input_pointer = unsafe { input_ptr.offset((j * input_stride) as isize) };
+            let mut ptr = input_pointer;
+            for _ in 0..(n / 8) {
+                let slice = unsafe { std::slice::from_raw_parts(ptr, 8) };
+                sum += slice.iter().sum::<f32>();
+                ptr = unsafe { ptr.offset(8) };
+            }
+            unsafe {
+                *output_location = sum / (n as f32);
+            }
+        }
+
+        return result.into_pyarray(py);
+    }
+
+    #[pyfn(m)]
+    #[pyo3(name = "mean_native_transpose_blas")]
+    fn mean_native_transpose_blas<'py>(
+        py: Python<'py>,
+        x: PyReadonlyArrayDyn<'py, f32>,
+    ) -> &'py PyArrayDyn<f32> {
+        // Assume that the input is transposed, so it is a 2D array with shape (d, n)
+
+        let dims = x.shape();
+        if dims.len() != 2 {
+            panic!("Expected a 2D array!");
+        }
+
+        let x = x.as_array();
+        let d = dims[0];
+        let n = dims[1];
+
+        let mut result = ArrayD::<f32>::zeros(vec![dims[1]]);
+        let input_ptr = x.as_ptr();
+        let input_stride = x.strides()[0] as usize;
+        let result_view = result.as_mut_ptr();
+        for j in 0..d {
+            let output_location = unsafe { result_view.offset((j) as isize) };
+            let input_pointer = unsafe { input_ptr.offset((j * input_stride) as isize) };
+            let data_vec: &[f32] = unsafe { std::slice::from_raw_parts(input_pointer, n) };
+
+            unsafe {
+                *output_location = sasum(n as i32, data_vec, 1);
+            }
+        }
+
+        return result.into_pyarray(py);
+    }
+
+    #[pyfn(m)]
+    #[pyo3(name = "mean_native_blas")]
+    fn mean_native_blas<'py>(
+        py: Python<'py>,
+        x: PyReadonlyArrayDyn<'py, f32>,
+    ) -> &'py PyArrayDyn<f32> {
+        // Assume that the input is transposed, so it is a 2D array with shape (d, n)
+
+        let dims = x.shape();
+        if dims.len() != 2 {
+            panic!("Expected a 2D array!");
+        }
+
+        let x = x.as_array();
+        let n = dims[0];
+        let d = dims[1];
+
+        // Allocate a new array to store the result
+        let mut result = ArrayD::<f32>::zeros(vec![dims[1]]);
+        let input_ptr = x.as_ptr();
+        let input_stride = x.strides()[0] as usize;
+        let input_slice = unsafe { std::slice::from_raw_parts(input_ptr, n * input_stride) };
+        let result_view = result.as_mut_ptr();
+        for j in 0..d {
+            let output_location = unsafe { result_view.offset((j) as isize) };
+
+            unsafe {
+                *output_location = sasum(n as i32, input_slice, input_stride as i32);
+            }
+        }
+
+        return result.into_pyarray(py);
     }
 
     Ok(())
